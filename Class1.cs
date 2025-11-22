a0203ba185882dfc466d2ccb2aeda9cae1f3539c
awmrozek <mrozekss@gmail.com>
2025-11-22 20:17:01 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyLibrary1;
+
+public class Class1
+{
+    async public void MyMethod ()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("PSQL_CONNECTION_STRIN"); ;
+        await using var dataSource = Npgsql.NpgsqlDataSource.Create(connectionString);
+
+
+        // Retrieve all rows
+        await using (var cmd = dataSource.CreateCommand("SELECT * FROM public.consolidated_communication_view;"))
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                Console.WriteLine(reader.GetString(0) + " " + reader.GetDateTime(1) + " " + reader.GetString(2));
+            }
+        }
+    }
+}
